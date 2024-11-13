67415bd9e12ec0b682e65ad8678af36f3ddae83a
mauldinc <mauldinc0609@student.faytechcc.edu>
2024-11-12 21:00:06 -0500

    Add files via upload

---
+++ P1Lab1_MauldinChristina.py
@@ -0,0 +1,19 @@
+# Christina Mauldin
+
+# 11/12/2024
+
+#P1Lab1
+
+# Getting user input and then displaying a message
+
+
+
+# get the users first name
+firstName = input("Enter your first name: ")
+
+# Get users last name
+lastName = input("Enter your last name: ")
+
+#Diplay a meesage after user puts in information
+print(f"Hello, {firstName} {lastName}! Welcome to CTI-110")
+            
